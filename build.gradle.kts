54daf321ac27000f726c10845b63f2f208718d32
gebo <35388172+gebogebogebo@users.noreply.github.com>
2024-05-20 08:36:24 +0900

    UPDATE

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity5")
-	implementation("com.yubico:webauthn-server-core:2.5.0")
+	implementation("com.yubico:webauthn-server-core:2.5.2")
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
